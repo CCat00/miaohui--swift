3f82b02d6c4d1b851f571a1f2564abeb66d3bdce
1019260351@qq.com <1019260351@qq.com>
2016-10-19 18:29:12 +0800

    tabbar

---
+++ 妙汇/MHTabBarController.swift
@@ -0,0 +1,85 @@
+//
+//  MHTabBarController.swift
+//  妙汇
+//
+//  Created by 韩威 on 2016/10/19.
+//  Copyright © 2016年 韩威. All rights reserved.
+//
+
+import UIKit
+
+class MHTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let images_normal = [
+            UIImage.init(named: "tab_home_wt"),
+            UIImage.init(named: "tab_Category_wt"),
+            UIImage.init(named: "tab_fieldOfView_wt"),
+            UIImage.init(named: "tab_cart_wt"),
+            UIImage.init(named: "tab_personal_wt"),
+            ]
+        
+        let images_selected = [
+            UIImage.init(named: "tab_home"),
+            UIImage.init(named: "tab_Category"),
+            UIImage.init(named: "tab_fieldOfView"),
+            UIImage.init(named: "tab_cart"),
+            UIImage.init(named: "tab_personal"),
+            ]
+        
+        let titles = [
+        "首页",
+        "分类",
+        "视野",
+        "购物车",
+        "我的妙汇"
+        ]
+
+        for (index, value) in self.tabBar.items!.enumerated() {
+            
+            var img_normal = images_normal[index]
+            var img_selected = images_selected[index]
+            let title = titles[index]
+            
+            img_normal = img_normal?.withRenderingMode(.alwaysOriginal)
+            img_selected = img_selected?.withRenderingMode(.alwaysOriginal)
+            
+            value.title = title
+            value.selectedImage = img_selected
+            value.image = img_normal
+            
+        }
+
+        self.tabBar.tintColor = MH_MAIN_COLOR_YELLOW
+        UITabBarItem.appearance().titlePositionAdjustment = UIOffset.init(horizontal: 0, vertical: -4)
+        UITabBarItem.appearance().setTitleTextAttributes(
+            [NSFontAttributeName : UIFont.systemFont(ofSize: 10.0),
+             NSForegroundColorAttributeName : UIColor.white
+            ], for: .normal)
+        UITabBarItem.appearance().setTitleTextAttributes(
+            [NSFontAttributeName : UIFont.systemFont(ofSize: 10.0),
+             NSForegroundColorAttributeName : MH_MAIN_COLOR_YELLOW
+            ], for: .selected)
+
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
