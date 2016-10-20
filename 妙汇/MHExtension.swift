cbe696abf49eac060730c60a19c8d5c4f97469e6
1019260351@qq.com <1019260351@qq.com>
2016-10-20 15:46:04 +0800

    首页展示完毕

--- 妙汇/MHExtension.swift
+++ 妙汇/MHExtension.swift
@@ -23,4 +23,12 @@ extension UIColor {
             blue: CGFloat(b/255.0)
             , alpha: 1.0)
     }
+    
+    static func rgbColor(r: CGFloat, g: CGFloat, b: CGFloat) -> UIColor{
+        return UIColor.init(
+            red: CGFloat(r/255.0),
+            green: CGFloat(g/255.0),
+            blue: CGFloat(b/255.0)
+            , alpha: 1.0)
+    }
 }
--- 妙汇/MHHomePromotionsItem.swift
+++ 妙汇/MHHomePromotionsItem.swift
@@ -9,6 +9,11 @@
 import UIKit
 import Kingfisher
 
+enum CountDownType {
+    case title
+    case bottom
+}
+
 /// 促销商品的一个item
 class MHHomePromotionsItem: UIView {
     
@@ -17,20 +22,117 @@ class MHHomePromotionsItem: UIView {
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var bottomLabel: UILabel!
     
+    var timer: Timer?
+    
+    var countdownType = CountDownType.bottom
+    
     var pro: MHPromotion? {
         didSet {
-            if pro != nil {
-                name.text = pro!.ad_name
-                title.text = pro!.ad_title
-                imageView.kf.setImage(with: URL.init(string: pro!.ad_code!))
-                // TODO: attribute
-                bottomLabel.text = "距离结束"
+            if pro == nil { return }
+            
+            if oldValue != nil { return } //滚动tableview的情况 // TODO:这里可能会出现，下拉刷新的时候，不应return，先这样写吧！！
+            
+            name.text = pro!.ad_name
+            
+            imageView.kf.setImage(with: URL.init(string: pro!.ad_code!))
+            
+            restTime = Int(pro!.end_time!)! - Int(pro!.server_time!)!
+            
+            if pro!.promote_price != nil { // 有价格：title显示倒计时，bottom显示价格
+                
+                countdownType = .title
+                
+                let attributedText = NSMutableAttributedString.init(
+                    string: "¥" + pro!.shop_price! + "  ",
+                    attributes: [
+                        NSFontAttributeName : UIFont.boldSystemFont(ofSize: 12.0),
+                        NSForegroundColorAttributeName: UIColor.rgbColor(r: 222.0, g: 43.0, b: 48.0),
+                        ])
+                
+                let oldPrice = NSAttributedString.init(
+                    string: " ¥" + pro!.promote_price!,
+                    attributes: [
+                        NSFontAttributeName : UIFont.systemFont(ofSize: 11.0),
+                        NSForegroundColorAttributeName: UIColor.rgbColor(r: 178, g: 178, b: 178),
+                        NSStrikethroughStyleAttributeName : NSNumber.init(value: NSUnderlineStyle.styleSingle.rawValue),
+                        NSStrikethroughColorAttributeName : UIColor.rgbColor(r: 178, g: 178, b: 178)
+                    ])
+                
+                attributedText.append(oldPrice)
+                
+                bottomLabel.attributedText = attributedText
+                
+                self.showCountdown()
             }
+            else { // 无价格：title显示title，bottom显示倒计时
+                
+                countdownType = .bottom
+                
+                title.text = pro!.ad_title
+                
+                self.showCountdown()
+                
+            }
+        }
+
+        
+    }
+    
+    private func showCountdown() {
+        if timer != nil {
+            timer!.invalidate()
+            timer = nil
+        }
+        timer = Timer.init(timeInterval: 1, target: self, selector: #selector(MHHomePromotionsItem.timerAction), userInfo: nil, repeats: true)
+        RunLoop.current.add(timer!, forMode: RunLoopMode.commonModes)
+    }
+    
+    @objc private func timerAction() {
+        if pro == nil { return }
+        restTime! -= 1
+        
+        if countdownType == .bottom {
+            bottomLabel.attributedText = jointCountdownString()
+        } else {
+            title.attributedText = jointCountdownString()
         }
     }
     
-    override func awakeFromNib() {
-        super.awakeFromNib()
+    var restTime: Int?
+    
+    private func jointCountdownString() -> NSAttributedString{
+        let attributedText = NSMutableAttributedString.init(
+            string: "距离结束  ",
+            attributes: [
+                NSFontAttributeName : UIFont.systemFont(ofSize: 12.0),
+                NSForegroundColorAttributeName: UIColor.rgbColor(r: 109, g: 109, b: 109)])
         
+        let timeString = turnToTime(with: TimeInterval(restTime!))
+        
+        let countdonwColor = countdownType == .title ? UIColor.black : UIColor.rgbColor(r: 252, g: 187, b: 53)
+        attributedText.append(NSAttributedString.init(
+            string: timeString,
+            attributes: [
+                NSFontAttributeName : UIFont.boldSystemFont(ofSize: 11.0),
+                NSForegroundColorAttributeName: countdonwColor
+            ]))
+        return attributedText
+    }
+    
+    private func turnToTime(with timestmp: TimeInterval) -> String{
+        
+        if timestmp > (24*60*60) {
+            //大于一天
+            
+            return "\(Int(timestmp / (24*60*60)))天"
+        }
+        
+        let dateFormatter = DateFormatter.init()
+        dateFormatter.dateFormat = "HH:mm:ss"
+        
+        let date = Date.init(timeIntervalSince1970: timestmp)
+        
+        let dateString = dateFormatter.string(from: date)
+        return dateString
     }
 }
